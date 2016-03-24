c0ca25f2943b7b2175467e0a606a6e045f1eb2e7
Evan Schultz <evan@rangle.io>
2016-03-24 13:17:55 -0700

    Lunch checkin

--- app/src/app.ts
+++ app/src/app.ts
@@ -5,12 +5,16 @@ import 'koast-angular';
 import 'basscss/css/basscss.css';
 import 'font-awesome/css/font-awesome.css';
 import '../css/styles.css';
+import {MainComponent} from './components/main/main-component';
 
 import * as angular from 'angular';
 import * as Rx from 'rx';
 
-angular.module('ngcourse', []);
+
+
+angular.module('ngcourse', [])
+  .directive(MainComponent.selector, MainComponent.directiveFactory);
 
 angular.element(document).ready(
-  () => angular.bootstrap(document, ['ngcourse'])
+    () => angular.bootstrap(document, ['ngcourse'])
 );
